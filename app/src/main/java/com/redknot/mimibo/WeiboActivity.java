0c780888482fed29d4b0894009ea2f928af183b9
redknot <redknotmiaoyuqiao@gmail.com>
2016-02-12 21:40:40 +0800

    weibiActivity

---
+++ app/src/main/java/com/redknot/mimibo/WeiboActivity.java
@@ -0,0 +1,9 @@
+package com.redknot.mimibo;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by miaoyuqiao on 16/2/12.
+ */
+public class WeiboActivity extends AppCompatActivity {
+}
